5eb8a95d1ff096cc2ccb00f0b0d4845cc3818ce0
VEDAT <vedatbasaq@gmail.com>
2025-07-12 16:01:48 +0300

    homework 11

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig(({ command }) => {
   return {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
+      base: '/goit-js-hw-11/',
     },
     root: 'src',
     build: {
